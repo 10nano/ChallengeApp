f7130db71134db9a123de5e0964f6cd6354d211c
10nano <jerzy.wrobel@protonmail.com>
2023-12-02 12:07:09 +0100

    Day 4 homework ready! (bool)

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,19 +1,18 @@
 ﻿// Ustawianie danych
 var name = "Ewa";
 var age = 33;
-// 'W' = Woman, 'M' = Man
-var sex = 'W';
+var isWoman = true;
 
 // weryfikacja i wyswietlanie
-if (sex == 'W' && age < 30)
+if (isWoman && age < 30)
 {
     Console.WriteLine("Kobieta poniżej 30 lat");
 }
-else if (name == "Ewa" && age == 33 && sex == 'W')
+else if (name == "Ewa" && age == 33 && isWoman)
 {
     Console.WriteLine("Ewa, lat 33");
 }
-else if (sex == 'M' && age < 18)
+else if (!isWoman && age < 18)
 {
     Console.WriteLine("Niepełnoletni Mężczyzna");
 }
