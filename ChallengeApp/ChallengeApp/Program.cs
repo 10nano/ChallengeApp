369cbdab1a2e77eb0a96af77285a13ae3abbbb95
10nano <jerzy.wrobel@protonmail.com>
2023-12-04 01:11:59 +0100

    Zadanie z dnia 5 - gotowe (bez podpowiedzi)

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,35 +1,35 @@
 ﻿/*
- * Zadanie zrobione bez podpowiedzi było pouczające.
- * Zaraz idę zobaczyć podpowiedź
+ * Zadanie zrobione bez podpowiedzi - było pouczające.
+ * Zaraz idę zobaczyć podpowiedź...
 */
-const int allDigitsCount = 10;
 
-ulong number = 45664157566; // Dana liczba
+const int NumberOfAllDigits = 10;
+
+ulong number = 45664157566; // Dana liczba.
 
 string numberInString = number.ToString();
 char[] letters = numberInString.ToCharArray();
 
-int[] digit = new int[allDigitsCount];
+int[] digit = new int[NumberOfAllDigits];
 
-foreach (var item in digit)
+foreach (int item in digit)
 {
-    digit[item] = 0; // Zerowania wystąpień każdej cyfry
+    digit[item] = 0; // Zerowania wystąpień każdej cyfry.
 }
 
-foreach (var item in letters)
+foreach (char item in letters)
 {
     // Bezpośrednia konwersja znaku cyfry typu char na cyfrę typu int daje 
     // reprezentacje liczbwą danej cyfry w ASCII, dlatego po odjęciu wartości
     // ASCII zera otrzymuję właściwą wartość cyfry typu int np.: '7' -> 7
     int i = item - '0';
 
-    ++digit[i]; // Zliczanie wystąpień każdej cyfry
-
+    ++digit[i]; // Zliczanie wystąpień każdej cyfry.
 }
 
 Console.WriteLine($"Wyniki dla liczby {number}\n");
 
-for (int i = 0; i < allDigitsCount; i++)
+for (int i = 0; i < NumberOfAllDigits; i++)
 {
     Console.WriteLine($"Cyfra {i} występuje {digit[i]} krotnie");
 }
