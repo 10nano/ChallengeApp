569b4e06cf2ae15f3dedc223b2bee6ea0ce0b5cc
10nano <jerzy.wrobel@protonmail.com>
2023-12-06 12:03:40 +0100

    Klasa Employee - pierwsze podejście

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,36 @@
+﻿/// 1. Stwórz klasę Employee, w której przechowasz imię, nazwisko, wiek
+/// oraz punkty pracownika w postaci liczb całkowitych
+/// 
+/// name surname age score
+
+
+namespace ChallengeApp
+{
+    public class Employee
+    {
+        private List<int> score = new List<int>();
+        public Employee(string name, string surname) // Konstruktor
+        {
+            this.Name = name;
+            this.Surname = surname;
+        }
+
+        public string Name { get; private set; } // Property (pole, właściwość)
+
+        public string Surname { get; private set; } // Property
+
+        public int Result // Metoda
+        {
+            get
+            {
+                return this.score.Sum();
+            }
+        }
+
+        public void AddScore(int number) // Metoda
+        {
+            this.score.Add(number);
+        }
+    }
+
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,8 +1,18 @@
-﻿using ChallengeApp;
+﻿/// 1. Stwórz klasę Employee, w której przechowasz imię, nazwisko, wiek
+/// oraz punkty pracownika w postaci liczb całkowitych
+/// 
+/// 2. Stwórz trzech pracowników i każdemu przydziel po 5 ocen z zakresu
+/// od 1 do 10
+/// 
+/// 3. Napisz program, który wyszuka pracownika z najwyższą liczbą ocen
+/// a następnie wyświetli jego dane oraz wynik.
+/// 
 
-var user1 = new User("Adam", "463634523");
-var user2 = new User("Zuzia", "463634523");
-var user3 = new User("Damian", "463634523");
+using ChallengeApp;
+
+var user1 = new Employee("Adam", "Kowalski");
+var user2 = new Employee("Zuzia", "Nowak");
+var user3 = new Employee("Damian", "Zientara");
 
 user1.AddScore(2);
 user1.AddScore(5);
