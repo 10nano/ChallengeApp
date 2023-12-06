7cf7f6dbf13dc5de293c25eab9877fdec9d51601
10nano <jerzy.wrobel@protonmail.com>
2023-12-06 20:42:05 +0100

    W21 zadanie domowe z dnia 6

--- ChallengeApp/ChallengeApp/Employee.cs
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -1,25 +1,22 @@
-﻿/// 1. Stwórz klasę Employee, w której przechowasz imię, nazwisko, wiek
-/// oraz punkty pracownika w postaci liczb całkowitych
-/// 
-/// name surname age score
-
-
-namespace ChallengeApp
+﻿namespace ChallengeApp
 {
     public class Employee
     {
         private List<int> score = new List<int>();
-        public Employee(string name, string surname) // Konstruktor
+        public Employee(string name, string surname, int age)
         {
             this.Name = name;
             this.Surname = surname;
+            this.Age = age;
         }
 
-        public string Name { get; private set; } // Property (pole, właściwość)
+        public string Name { get; private set; }
 
-        public string Surname { get; private set; } // Property
+        public string Surname { get; private set; }
 
-        public int Result // Metoda
+        public int Age { get; private set; }
+
+        public int Result
         {
             get
             {
@@ -27,7 +24,7 @@ namespace ChallengeApp
             }
         }
 
-        public void AddScore(int number) // Metoda
+        public void AddScore(int number)
         {
             this.score.Add(number);
         }
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -10,11 +10,40 @@
 
 using ChallengeApp;
 
-var user1 = new Employee("Adam", "Kowalski");
-var user2 = new Employee("Zuzia", "Nowak");
-var user3 = new Employee("Damian", "Zientara");
+var user1 = new Employee("Adam", "Kowalski", 44);
+var user2 = new Employee("Zuzia", "Nowak", 23);
+var user3 = new Employee("Damian", "Zientarski", 33);
 
-user1.AddScore(2);
 user1.AddScore(5);
-int result = user1.Result;
-Console.WriteLine(result);
+user2.AddScore(6);
+user3.AddScore(7);
+user1.AddScore(8);
+user2.AddScore(9);
+user3.AddScore(10);
+user1.AddScore(1);
+user2.AddScore(2);
+user3.AddScore(3);
+user1.AddScore(4);
+user2.AddScore(5);
+user3.AddScore(6);
+user1.AddScore(7);
+user2.AddScore(8);
+user3.AddScore(9);
+
+Employee bestEmployee;
+
+if (user1.Result > user2.Result && user1.Result > user3.Result)
+{
+    bestEmployee = user1;
+}
+else if (user2.Result > user3.Result)
+{
+    bestEmployee = user2;
+}
+else
+{
+    bestEmployee = user3;
+}
+
+Console.WriteLine($"W tym miesiącu z wynikiem {bestEmployee.Result} najlepszym pracownikiem został:");
+Console.WriteLine($"{bestEmployee.Name} {bestEmployee.Surname} lat {bestEmployee.Age}");
