4627f43dbc98ac41cbf2efa19ca72411ebc5e451
10nano <jerzy.wrobel@protonmail.com>
2023-12-17 22:16:51 +0100

    W21 zadanie domowe z dnia 6 ze wskazówką v2.1

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -13,30 +13,37 @@ using ChallengeApp;
 var emp1 = new Employee("Adam", "Kowalski", 44);
 var emp2 = new Employee("Zuzia", "Nowak", 23);
 var emp3 = new Employee("Damian", "Zientarski", 33);
+var emp4 = new Employee("Gerard", "Hohenzollern", 88);
 
 emp1.AddScore(5);
 emp2.AddScore(6);
 emp3.AddScore(7);
+emp4.AddScore(8);
 
 emp1.AddScore(8);
 emp2.AddScore(9);
 emp3.AddScore(10);
+emp4.AddScore(8);
+
 
 emp1.AddScore(1);
 emp2.AddScore(2);
 emp3.AddScore(3);
+emp4.AddScore(8);
 
 emp1.AddScore(4);
 emp2.AddScore(5);
 emp3.AddScore(6);
+emp4.AddScore(8);
 
 emp1.AddScore(7);
 emp2.AddScore(8);
 emp3.AddScore(9);
+emp4.AddScore(8);
 
 List<Employee> employees = new List<Employee>()
 {
-    emp1, emp2, emp3
+    emp1, emp2, emp3, emp4
 };
 
 int bestResult = -1;    
