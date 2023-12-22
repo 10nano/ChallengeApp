1314f87db5899d156714c4790a4dc81b605c196c
10nano <jerzy.wrobel@protonmail.com>
2023-12-22 21:40:43 +0100

    Dzień 9 - zadanie domowe - testy przechodzą OK

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,48 +1,13 @@
 ﻿using ChallengeApp;
 
-var emp1 = new Employee("Adam", "Kowalski", 44);
-var emp2 = new Employee("Zuzia", "Nowak", 23);
-var emp3 = new Employee("Damian", "Zientarski", 33);
-var emp4 = new Employee("Gerard", "Hohenzollern", 88);
+var employee = new Employee("Adam", "Kowalski");
 
-emp1.AddScore(5);
-emp2.AddScore(7);
-emp3.AddScore(7);
-emp4.AddScore(8);
+employee.AddScore(1);
+employee.AddScore(0);
+employee.AddScore(3);
 
-emp1.AddScore(8);
-emp2.AddScore(9);
-emp3.AddScore(10);
-emp4.AddScore(8);
+var statistics = employee.GetStatistics();
 
-emp1.AddScore(1);
-emp2.AddScore(2);
-emp3.AddScore(3);
-emp4.AddScore(8);
-
-emp1.AddScore(4);
-emp2.AddScore(5);
-emp3.AddScore(6);
-emp4.AddScore(8);
-
-emp1.AddScore(7);
-emp2.AddScore(8);
-emp3.AddScore(9);
-emp4.AddScore(8);
-
-List<Employee> employees = new List<Employee>()
-{
-    emp1, emp2, emp3, emp4
-};
-
-var stat1 = emp1.GetStatistics();
-var stat2 = emp2.GetStatistics();
-
-Console.WriteLine($"Average: {stat1.Average:N2}");
-Console.WriteLine($"Min: {stat1.Min}");
-Console.WriteLine($"Max: {stat1.Max}");
-
-Console.WriteLine($"Average: {stat2.Average:N2}");
-Console.WriteLine($"Min: {stat2.Min}");
-Console.WriteLine($"Max: {stat2.Max}");
-
+Console.WriteLine($"Average: {statistics.Average:N2}");
+Console.WriteLine($"Min: {statistics.Min}");
+Console.WriteLine($"Max: {statistics.Max}");
